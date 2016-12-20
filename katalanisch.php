211db688f41d2195d11abf4dd59681d764264fb9
Yuserys <kontakt@yusel.net>
2016-12-20 05:28:38 +0100

    Update katalanisch.php

--- katalanisch.php
+++ katalanisch.php
@@ -12,4 +12,41 @@ $tpl = new Smarty;
 require_once('./classes/Database.class.php');
 require_once('./classes/Wortschatz.class.php');
 require_once('./inc/config.php');
+
+// Initiliaze necessary variables
+$baseLanguage = "katalanisch";
+$topic = ($_GET["topic"] != "") ? $_GET["topic"] : "";
+
+// Topics in $baseLanguage
+// NOTE: A database table 'topics' would be far better. ~Yuserys
+$myTopics = array(
+	'kalendar' 	=> array(
+		'short' 	=> "cal",
+		'name' 		=> "Kalendar",
+		'active' 	=> false,
+	),
+	'schule' 	=> array(
+		'short'		=> "edu",
+		'name'		=> "Schule und Ausbildung",
+		'active'	=> false,
+	),
+	'religion'	=> array(
+		'short'		=> "rel",
+		'name'		=> "Religion",
+		'active'	=> false,
+	),
+);
+
+// Check which topic is active
+foreach($aTopic in $myTopics) {
+	if($topic == $aTopic['short'])
+		$aTopic['active'] == true;
+}
+
+// Assign variables
+$tpl->assign('baseLang', $baseLanguage);
+$tpl->assign('myTopics', $myTopics);
+
+// Load template
+$tpl->display('katalanisch.tpl');
 ?>
