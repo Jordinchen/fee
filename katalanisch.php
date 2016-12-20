2536fedb1df1d1d94f6fab793aa47af0010dcf27
Yuserys <kontakt@yusel.net>
2016-12-20 04:59:47 +0100

    Create katalanisch.php

---
+++ katalanisch.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Jordinchen, Kevin Sporbeck
+ * @version 20.12.2016
+ */
+
+// Include Smarty
+require_once('./smarty/libs/Smarty.class.php');
+$tpl = new Smarty;
+
+// Include necessary configuration files
+require_once('./classes/Database.class.php');
+require_once('./classes/Wortschatz.class.php');
+require_once('./inc/config.php');
+?>
