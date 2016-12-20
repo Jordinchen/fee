690b35f7e7ae23ca9e05b728c926753499921be0
Yuserys <kontakt@yusel.net>
2016-12-20 03:34:49 +0100

    Update index.php
    Included Smarty; removed "?page="-structure

--- index.php
+++ index.php
@@ -1,38 +1,18 @@
-<?php 
+<?php
 /**
  * @author Jordinchen, Kevin Sporbeck
  * @version 20.12.2016
  */
 
-// Include necessary files
-require_once("./classes/Database.class.php");
-require_once("./inc/config.php");
+// Include Smarty
+require_once('./smarty/libs/Smarty.class.php');
+$tpl = new Smarty;
 
+// Include necessary configuration files
+require_once('./classes/Database.class.php');
+require_once('./classes/Wortschatz.class.php');
+require_once('./inc/config.php');
 
-/**************/
-include_once("funktionen.php");
-include("header.php");
-include("nav.php");
-if(!(isset($_GET['page'])) or (isset($_GET['page']) && $_GET['page'] == "index")) {
-	include("index2.php");
-} else { 
-	if(isset($_GET['page']) && $_GET['page'] == "catale") {
-		include("katalanisch.php");
-	} else { 
+// Display template
+$tpl->display('index.tpl');
 ?>
-		<div class="container runter">
-			<div class="row">
-				<div class="col-sm-3">
-				</div>
-				<div class="col-sm-9">
-					<h1>Kein Inhalt verfügbar</h1>
-				</div>
-			</div>
-		</div>
-<?php
-	}
-}
-?>
-
-</body>
-</html>
